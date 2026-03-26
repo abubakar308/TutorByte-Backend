47f4b0b686888c533f42e7ab135ae6d0b7f5fcb8
abubakar308 <mdabubakarsiddique789@gmail.com>
2026-03-26 15:53:42 +0600

    betterauth install and schema generated

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, { Application, Request, Response } from 'express';
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./app/lib/auth";
 
 const app: Application = express();
 
 app.use(express.json());
+
+app.all("/api/auth/*", toNodeHandler(auth));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('TutorByte');
